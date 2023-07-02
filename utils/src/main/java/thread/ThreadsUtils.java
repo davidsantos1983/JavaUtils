47d38e3d3d3837fa131fbb54d22db287ce6f84fb
davidsantos1983 <david.jsantos1983@gmail.com>
2023-07-02 16:06:09 +0100

    Adaptado ao intellij

--- utils/src/main/java/thread/ThreadsUtils.java
+++ utils/src/main/java/thread/ThreadsUtils.java
@@ -4,41 +4,46 @@ import java.util.List;
 
 public class ThreadsUtils {
 
-	public static void runAll(List<Thread> threads) {
-		
-		for (Thread thread : threads) {
+    public static void runAll(List<Thread> threads) {
 
-			thread.start();
-		}
-	}
+        for (Thread thread : threads) {
 
-	/**
-	 * @param threads
-	 * @return false return error, true all threads join;
-	 */
-	public static boolean waitAll(List<Thread> threads) {
+            thread.start();
+        }
+    }
 
-		for (Thread thread : threads) {
+    /**
+     * @param threads
+     * @return false return error, true all threads join;
+     */
+    public static boolean waitAll(List<Thread> threads) {
 
-			try {
+        for (Thread thread : threads) {
 
-				thread.join();
+            try {
 
-			} catch (InterruptedException e) {
+                thread.join();
 
-				return false;
-			}
-		}
+            } catch (InterruptedException e) {
 
-		return true;
-	}
+                return false;
+            }
+        }
 
-	@SuppressWarnings("deprecation")
-	public static void stopAll(List<Thread> threads) {
+        return true;
+    }
 
-		for (Thread thread : threads) {
+    @SuppressWarnings("deprecation")
+    public static void stopAll(List<Thread> threads) {
 
-			thread.stop();
-		}
-	}
+        try {
+
+            for (Thread thread : threads) {
+
+                thread.stop();
+            }
+        } catch (Exception e) {
+            // Se der exception deu
+        }
+    }
 }
